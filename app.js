ae4d67c493f1db7a32055c75ea43ac25c6aeebd0
megacartridge <mdltrack@gmail.com>
2016-11-15 18:39:34 -0500

    Remove for loop. Add door buttons. Add door button functions.

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var winningcount = 0;
 
 var newUserChoice; //Final choice user will make.
 
-for (i=0; i<10000; i++) {
+var cardoor=0;
+
+
+
     //Initialize the three doors. Get rid or "door zero."
     var doorChoices = [0,1,2,3];
     doorChoices.shift();
@@ -11,7 +14,20 @@ for (i=0; i<10000; i++) {
     //var firstUserChoice = 1;
     //var firstUserChoice = 2;
     //var firstUserChoice = 3;
+
+    function Door1() {
+        console.log("You picked door 1!");
+    };
+
+    function Door2() {
+        console.log("You picked door 2!");
+    };
+
+    function Door3() {
+        console.log("You picked door 3!");
+    };
    
+    /*
     console.log("The user chooses " + firstUserChoice);
 
     //Remove firstUserChoice from doorChoices
@@ -22,7 +38,7 @@ for (i=0; i<10000; i++) {
     var cardoor= Math.floor((Math.random() *3) + 1);
     console.log("The car is behind " + cardoor);
 
-/*
+
     //Newdoorpicker randomly selects door 2 or 3.
     var newDoorPicker = Math.floor((Math.random() *100) + 1);
 
@@ -34,12 +50,14 @@ for (i=0; i<10000; i++) {
 
     console.log("Final user choice is " + newUserChoice);
 */
-    if (cardoor == firstUserChoice) {
+    //User always changes his choice to either 2 or 3.
+    if (cardoor == newUserChoice) {
         winningcount = (winningcount + 1);
+        console.log(winningcount);
         console.log("You win the car!!!");
     }; 
 
     console.log(" ");
-};
+
 
 alert("You won the car " + winningcount + " times!")
